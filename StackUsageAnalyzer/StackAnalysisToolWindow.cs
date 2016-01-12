2c424e785f53a445102c4cdb2eaec6fd52849816
Olsen, Morten Engelhardt <Morten_engelhardt.Olsen@atmel.com>
2016-01-12 17:23:03 +0100

    Add window, package and command class

---
+++ StackUsageAnalyzer/StackAnalysisToolWindow.cs
@@ -0,0 +1,40 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="StackAnalysisToolWindow.cs" company="Microsoft">
+//     Copyright (c) Microsoft.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace StackUsageAnalyzer
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("ffbd67c0-1100-4fa5-8c20-cc10264c540b")]
+    public class StackAnalysisToolWindow : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StackAnalysisToolWindow"/> class.
+        /// </summary>
+        public StackAnalysisToolWindow() : base(null)
+        {
+            this.Caption = "StackAnalysisToolWindow";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new StackAnalysisToolWindowControl();
+        }
+    }
+}
---
+++ StackUsageAnalyzer/StackAnalysisToolWindowControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="StackAnalysisToolWindowControl.xaml.cs" company="Microsoft">
+//     Copyright (c) Microsoft.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace StackUsageAnalyzer
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for StackAnalysisToolWindowControl.
+    /// </summary>
+    public partial class StackAnalysisToolWindowControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StackAnalysisToolWindowControl"/> class.
+        /// </summary>
+        public StackAnalysisToolWindowControl()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Handles click on the button by displaying a message box.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event args.</param>
+        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this),
+                "StackAnalysisToolWindow");
+        }
+    }
+}
