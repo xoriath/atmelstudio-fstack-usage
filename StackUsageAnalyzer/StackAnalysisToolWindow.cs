54bae40f921a4433c5c7caf94ab78e0f7c3f2672
Olsen, Morten Engelhardt <Morten_engelhardt.Olsen@atmel.com>
2016-01-14 21:40:48 +0100

    Change caption of Tool Window

--- StackUsageAnalyzer/StackAnalysisToolWindow.cs
+++ StackUsageAnalyzer/StackAnalysisToolWindow.cs
@@ -29,7 +29,7 @@ namespace StackUsageAnalyzer
         /// </summary>
         public StackAnalysisToolWindow() : base(null)
         {
-            this.Caption = "StackAnalysisToolWindow";
+            this.Caption = "Function Stack Analysis";
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
